3208fff0f7d2248162fae4ed9d3b47b15a1ed15c
Jahirul83 <jahirulislamcse83@gmail.com>
2023-10-18 15:19:07 +0600

    firebase setup

---
+++ src/components/SignUp.jsx
@@ -0,0 +1,10 @@
+
+const SignUp = () => {
+    return (
+        <div>
+            <h2>please signUp</h2>
+        </div>
+    );
+};
+
+export default SignUp;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDT-j_AOF6ABFPdTqj__mrqH7SpMZrQM9s",
+  authDomain: "coffee-store-f4d17.firebaseapp.com",
+  projectId: "coffee-store-f4d17",
+  storageBucket: "coffee-store-f4d17.appspot.com",
+  messagingSenderId: "397740835467",
+  appId: "1:397740835467:web:d28532bcfd7f26582eca8e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
