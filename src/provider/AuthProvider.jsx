9ac2c0dd2d1b8cbba7af94f0543f6475d02e7bab
Jahirul83 <jahirulislamcse83@gmail.com>
2023-10-19 03:10:28 +0600

    user update delete read

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,5 +1,5 @@
 import { createContext, useState } from "react";
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import app from "../firebase/firebase.config";
 const auth = getAuth(app);
 
@@ -8,12 +8,18 @@ export const AuthContext = createContext(null);
 
 const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
 
-    createUser(email,password){
-        return createUserWithEmailAndPassword(auth,email,password);
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+    const signInUser = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
     }
 
-    const userInfo = { user }
+    const userInfo = { user, loading, createUser, signInUser }
     return (
         <AuthContext.Provider value={userInfo}>
             {children}
